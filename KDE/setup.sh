482e93dcac5df040d9238559ceb4a2b4562c7dde
Bart Kessels <b.kessels@bk-mail.com>
2016-04-17 23:01:19 +0200

    PHP7, add symlink from apache root to home folder

--- KDE/setup.sh
+++ KDE/setup.sh
@@ -43,11 +43,16 @@ zypper addrepo http://download.opensuse.org/repositories/Education/$os_version/ 
 # KDE:Extra
 zypper addrepo http://download.opensuse.org/repositories/KDE:/Extra/$os_version/ KDE:extra
 
-# Server
+# PHP
 zypper addrepo http://download.opensuse.org/repositories/server:/php:/applications/$os_version/ php:applications
+zypper addrepo http://download.opensuse.org/repositories/devel:/languages:/php/$os_version/ php7:applications
 
 # X11 utils
 zypper addrepo http://download.opensuse.org/repositories/X11:/Utilities/$os_version/ X11:Utils 
+
+# Refresh REPO list
+printf '\n\nPlease enter the "a" key to add the REPO\n'
+zypper refresh
 
 #####################################################################################
 #####################################################################################
@@ -58,25 +63,26 @@ zypper addrepo http://download.opensuse.org/repositories/X11:/Utilities/$os_vers
 #####################################################################################
 
 # Remove default created folders
-rm -rf /home/$user_name/public_html
-rm -rf /home/$user_name/bin
+rm -r /home/$user_name/bin
 
 # Create folders inside home dir
-mkdir /home/$user_name/bk-cloud
-mkdir /home/$user_name/Desktop
-mkdir /home/$user_name/Downloads
-mkdir /home/$user_name/Documents
-mkdir /home/$user_name/Music
-mkdir /home/$user_name/Pictures
-mkdir /home/$user_name/Videos
+mkdir -p /home/$user_name/bk-cloud
+mkdir -p /home/$user_name/Desktop
+mkdir -p /home/$user_name/Downloads
+mkdir -p /home/$user_name/Documents
+mkdir -p /home/$user_name/Music
+mkdir -p /home/$user_name/Pictures
+mkdir -p /home/$user_name/public_html
+mkdir -p /home/$user_name/Videos
 
 # Create folders inside /etc/skel dir
-mkdir /etc/skel/Desktop
-mkdir /etc/skel/Downloads
-mkdir /etc/skel/Documents
-mkdir /etc/skel/Music
-mkdir /etc/skel/Pictures
-mkdir /etc/skel/Videos
+mkdir -p /etc/skel/Desktop
+mkdir -p /etc/skel/Downloads
+mkdir -p /etc/skel/Documents
+mkdir -p /etc/skel/Music
+mkdir -p /etc/skel/Pictures
+mkdir -p /etc/skel/public_html
+mkdir -p /etc/skel/Videos
 
 #####################################################################################
 #####################################################################################
@@ -253,7 +259,7 @@ zypper install phpmyadmin
 sed -i -e '1iAlias /phpmyadmin /srv/www/htdocs/phpMyAdmin\' /etc/apache2/conf.d/phpMyAdmin.conf
 
 # Install PHP
-zypper install php-ZendFramework php-composer
+zypper install php7 php5-ZendFramework php-composer
 
 # Install laravel installer
 composer global require --working-dir=/home/$user_name/.composer "laravel/installer"
@@ -264,10 +270,11 @@ sed -i 's|Options None|Options Indexes FollowSymLinks|g' /etc/apache2/default-se
 a2enmod rewrite
 
 # Enable php module
-a2enmod php5
+a2enmod php7
 
 # Set PHP display_errors to on
 sed -i 's|display_errors = Off|display_errors = On|g' /etc/php5/apache2/php.ini
+sed -i 's|display_errors = Off|display_errors = On|g' /etc/php7/apache2/php.ini
 
 # Install Rails
 zypper install ruby2.1-rubygem-rails-4_2
@@ -275,9 +282,10 @@ zypper install ruby2.1-rubygem-rails-4_2
 # Install packages from npm
 npm install -g bower typescript grunt-cli
 
-# Read/write access for users in htdocs
-chown -R :users /srv/www/htdocs
-chmod 777 /srv/www/htdocs
+# Read/write access for user in own html folder
+ln -s /home/$user_name/public_html /srv/www/htdocs/$user_name
+
+chown -R $user_name /home/$user_name/public_html
 
 #####################################################################################
 #####################################################################################
