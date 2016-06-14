7c44fe4e2853408b0c63d3fa96eae09cb1cac853
Bart Kessels <b.kessels@bk-mail.com>
2016-06-14 08:31:45 +0200

    Add chromium-ffmpeg package

--- KDE/setup.sh
+++ KDE/setup.sh
@@ -116,7 +116,7 @@ mkfontdir
 zypper install kwayland-integration kwrited5 plasma5-workspace-wallpapers kdeclarative-tools krandr libKF5Screen6-32bit plasma5-workspace-libs-32bit kdm patterns-openSUSE-rest_cd_kde plasma5-openSUSE kcm_systemd
 
 # Codecs
-zypper install celt flac libdv speex wavpack k3b-codecs ffmpeg lame phonon-backend-vlc phonon4qt5-backend-vlc vlc-codecs libdvdcss2
+zypper install celt flac libdv speex wavpack k3b-codecs ffmpeg lame phonon-backend-vlc phonon4qt5-backend-vlc vlc-codecs libdvdcss2 chromium-ffmpeg
 
 # ZSH
 zypper install zsh
